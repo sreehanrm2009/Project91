01748e1ec356ecd8c0bb541eeb39b54adb4e8b0e
sreehanrm2009 <83212024+sreehanrm2009@users.noreply.github.com>
2021-07-30 21:01:47 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,5 +3,5 @@ function addUser() {
       p2 = document.getElementById("player2name").value;
       localStorage.setItem("p1n", p1);
       localStorage.setItem("p2n", p2);
-      window.display = "Game/index.html";
+      window.display = "./Game";
 }
